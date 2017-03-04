684e08f417f0e9b713db3993c7ddda81ec82f718
Hiroki Kumamoto <kumabook@live.jp>
2017-03-04 12:58:01 +0900

    Add comment parser

--- Sources/Parser+TOML.swift
+++ Sources/Parser+TOML.swift
@@ -2,7 +2,26 @@ import Runes
 import Result
 import TryParsec
 
-
+/// Parses TOML.
 public func parseTOML(_ str: String) -> Result<TOML, ParseError> {
-    return .failure(ParseError.message("Not implemented"))
+    return parseOnly(toml, str.unicodeScalars)
 }
+
+
+// MARK: Private
+internal let toml = _toml()
+private func _toml() -> Parser<String.UnicodeScalarView, TOML> {
+    return skipSpaces *> tomlValue <* skipSpaces <* endOfInput()
+}
+
+internal let tomlValue = _tomlValue()
+private func _tomlValue() -> Parser<String.UnicodeScalarView, TOML> {
+    return comment
+}
+
+internal let comment = _comment();
+private func _comment() -> Parser<String.UnicodeScalarView, TOML> {
+    return string("#") *> manyTill(any, string("\n"))
+      <&> { .comment(String($0)) }
+}
+
