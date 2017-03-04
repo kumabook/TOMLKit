ac666c8ae59518eb2b6799a333b98f2959b6962e
Hiroki Kumamoto <kumabook@live.jp>
2017-03-04 12:57:43 +0900

    Add Quick and Nimble as dependency

--- Package.swift
+++ Package.swift
@@ -4,5 +4,7 @@ let package = Package(
     name: "TOMLKit",
     dependencies: [
       .Package(url: "https://github.com/kumabook/TryParsec.git", "0.1.2"),
+      .Package(url: "https://github.com/Quick/Quick.git", "1.1.0"),
+      .Package(url: "https://github.com/Quick/Nimble.git", "6.0.1"),
     ]
 )
